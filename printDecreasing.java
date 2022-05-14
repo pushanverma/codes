08dd673dc8a2cbcdff731bb5c7161aa06c64b4c0
pushanverma <pushanverma.99@gmail.com>
2022-05-14 19:03:30 +0530

    added

---
+++ printDecreasing.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class printDecreasing
+{
+    public static void main(String [] args)
+    {
+        System.out.println("hello");
+        System.out.println("hello");
+        
+    }
+}
