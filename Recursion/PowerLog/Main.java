a6d9350b526f69504b1ae64763cefa15957d9b2f
pushanverma <pushanverma.99@gmail.com>
2022-05-20 00:48:15 +0530

    adding power Lograithmic

---
+++ Recursion/PowerLog/Main.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn =new Scanner(System.in);
+        int x=scn.nextInt();
+        int n =scn.nextInt();
+        int ans =power(x,n);
+        System.out.println(ans);
+        
+    }
+
+    public static int power(int x, int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        int recans =power(x,n/2);
+        int ans =recans*recans;
+        if(n%2!=0)
+        {
+            int ans1=ans*x;
+            return ans1;
+        }
+        
+        return ans;
+    }
+
+}
